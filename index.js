e1bfcf2a703bb85e5363d7419e4aeea57ac13dfc
Bob Scott <bob@rwscott.co.uk>
2020-09-28 21:12:37 +0100

    Add args

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const args = require('args');
 const fs = require('fs');
 const htmlparser = require('./modules/htmlparser');
 const xmlproc = require('./modules/xml');
 const xml = xmlproc.create();
 
-const html = fs.readFileSync('Raindrop.io.html', 'utf8');
+args
+    .option('input', 'The input file', 'Raindrop.io.html')
+    .option('output', 'The output file', 'stdout');
+
+const flags = args.parse(process.argv);
+
+const html = fs.readFileSync(flags.i, 'utf8');
 htmlparser.convert(html, (bookmark) => xmlproc.node(xml, bookmark));
-console.log(xmlproc.toString(xml));
+const output = xmlproc.toString(xml);
+if(flags.o === 'stdout') {
+    console.log(output);
+} else {
+    fs.writeFileSync(flags.o, output);
+}
+
