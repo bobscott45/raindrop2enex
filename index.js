b43a15b473f9f91c724b4e42ff406a5a52cdc874
Bob Scott <bob@rwscott.co.uk>
2020-09-29 08:04:44 +0100

    Remove commented out code

--- index.js
+++ index.js
@@ -13,18 +13,7 @@ const flags = args.parse(process.argv);
 const input = fs.createReadStream(flags.f);
 const output = fs.createWriteStream(flags.o);
 
-//const html = fs.readFileSync(flags.f, 'utf8');
-
 input.on('data', chunk => htmlparser.convert(chunk, (bookmark) => xmlproc.node(xml, bookmark)));
 input.on('end', () => xmlproc.write(xml, output));
 
-//  htmlparser.convert(html, (bookmark) => xmlproc.node(xml, bookmark));
-
-// const output = xmlproc.toString(xml);
-//
-// if(flags.o === 'stdout') {
-//     console.log(output);
-// } else {
-//     fs.writeFileSync(flags.o, output);
-// }
 
