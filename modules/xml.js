298ecde7a288087f31e1efc0f217c306c60c599f
robert <bob@rwscott.co.uk>
2020-09-30 15:40:38 +0100

    Suppress empty tag

--- modules/xml.js
+++ modules/xml.js
@@ -26,7 +26,9 @@ exports.node = function(xml, bookmark) {
     note.ele('content').dat(content(bookmark.url, bookmark.description));
     note.ele('created').txt(bookmark.date_added);
     note.ele('updated').txt(bookmark.last_modified);
-    note.ele('tag').txt(bookmark.container);
+    if(bookmark.container) {
+        note.ele('tag').txt(bookmark.container);
+    }
     if(bookmark.tags !== '') {
         let tags = bookmark.tags.split(',');
         tags.forEach(tag => note.ele('tag').txt(tag));
